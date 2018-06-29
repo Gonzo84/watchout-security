b10035443415ae030379e174241b0660eee4251e
vdukic1586@gmail.com <testpass>
2018-06-29 16:56:54 +0200

    Redesign site

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => (
 
         <div className="ws-header ws-gradient">
             <div className="container">
-                <div className="ws-slider-cont">
+                <div className="ws-slider-cont ws-header-no-slider">
                     <div className={classes.Header} style={{backgroundImage: 'url(' + props.backgroundImg + ')'}}>
                         <h4>{props.title}</h4>
                         <p>{props.description}</p>
--- src/containers/home/headerItem/HeaderItem.js
+++ src/containers/home/headerItem/HeaderItem.js
@@ -8,21 +8,21 @@ const HeaderItem = (props) => (
 
         <div className="row">
 
-            <div className="col-md-6">
+            <div className="col-md-8 ws-slider-info">
                 <h1>{props.bodyContent.title}</h1>
                 <h3>{props.bodyContent.content}</h3>
             </div>
 
-            <div className="col-md-6">
-                <div>
+            <div className="col-md-4 ws-slide-counters">
+                <div className="ws-slider-counter">
                     <h2>{props.bodyContent.widgets[0].number}</h2>
                     <p>{props.bodyContent.widgets[0].label}</p>
                 </div>
-                <div>
+                <div className="ws-slider-counter">
                     <h2>{props.bodyContent.widgets[1].number}</h2>
                     <p>{props.bodyContent.widgets[1].label}</p>
                 </div>
-                <div>
+                <div className="ws-slider-counter">
                     <h2>{props.bodyContent.widgets[2].number}</h2>
                     <p>{props.bodyContent.widgets[2].label}</p>
                 </div>
