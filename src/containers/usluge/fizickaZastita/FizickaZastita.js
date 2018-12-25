af2ffaa2c88eeaef5b617c4e0e2700dd97f2a2ca
@GonzoKg <krsticm@ymail.com>
2018-12-25 19:09:48 +0100

    fizicko obezbedjenje

--- src/containers/usluge/fizickaZastita/FizickaZastita.js
+++ src/containers/usluge/fizickaZastita/FizickaZastita.js
@@ -13,8 +13,8 @@ class FizickaZastita extends Component {
         window.scrollTo(0, 0);
         const headerConfig = {
             backgroundImg: fizickaZastitaImg,
-            title: 'FIZICKA ZASTITA',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu justo malesuada, vestibulum nibh sed, congue neque. Fusce id erat felis. Aenean vel tortor libero. Quisque vitae purus eget magna porta vehicula.'
+            title: 'Fizička zaštita',
+            description: 'Biti u isto vreme i smeo i veoma oprezan, umetnost je uspeha. Napoleon'
         };
         return (
             <Aux>
@@ -27,22 +27,8 @@ class FizickaZastita extends Component {
 
                         <div className="container">
                             <div className="ws-centered-info-block">
-                                <h6>FIZICKA ZASTITA</h6>
-                                <p>
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                                </p>
-                                <p>
-                                    when an unknown printer took a galley of type and scrambled it to make a type
-                                    specimen book.
-                                    It has survived not only five centuries, but also the leap into electronic
-                                    typesetting, remaining essentially unchanged.
-                                </p>
-                                <p>
-                                    when an unknown printer took a galley of type and scrambled it to make a type
-                                    specimen book.
-                                    It has survived not only five centuries, but also the leap into electronic
-                                    typesetting, remaining essentially unchanged.
+                                <p>Sistemski pristup u osiguravanju bezbednosti najvrednijih resursa u poslovanju, pored
+                                    tehničke podrazumeva i fizičku zaštitu.
                                 </p>
                             </div>
                         </div>
@@ -51,22 +37,8 @@ class FizickaZastita extends Component {
 
                     <div className="ws-simple-style-cont ws-alt ws-first">
                         <div className="container">
-                            <h3>FIZICKA ZASTITA</h3>
-                            <p className="ws-stronger-color">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                            </p>
-                            <p>
-                                when an unknown printer took a galley of type and scrambled it to make a type specimen
-                                book.
-                                It has survived not only five centuries, but also the leap into electronic typesetting,
-                                remaining essentially unchanged.
-                            </p>
-                            <p>
-                                when an unknown printer took a galley of type and scrambled it to make a type specimen
-                                book.
-                                It has survived not only five centuries, but also the leap into electronic typesetting,
-                                remaining essentially unchanged.
+                            <p>Fizičkim obezbeđenjem doprinosi se zaštiti ljudi i imovine od oštećenja, uništenja, krađe
+                                i drugih oblika štetnog delovanja.
                             </p>
                         </div>
                     </div>
@@ -74,88 +46,58 @@ class FizickaZastita extends Component {
 
                     <div className="ws-simple-style-cont">
                         <div className="container">
-                            <h3>FIZICKA ZASTITA</h3>
-                            <p className="ws-stronger-color">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                            </p>
-                            <p>
-                                when an unknown printer took a galley of type and scrambled it to make a type specimen
-                                book.
-                                It has survived not only five centuries, but also the leap into electronic typesetting,
-                                remaining essentially unchanged.
-                            </p>
-                            <p>
-                                when an unknown printer took a galley of type and scrambled it to make a type specimen
-                                book.
-                                It has survived not only five centuries, but also the leap into electronic typesetting,
-                                remaining essentially unchanged.
+                            <p>Usluga fizičke zaštite je usluga obezbeđenja koja se pruža ličnim prisustvom i
+                                neposrednom aktivnošću službenika obezbeđenja u određenom prostoru i vremenu, kao i
+                                primenom mera i upotrebom sredstava prinude.
                             </p>
                         </div>
                     </div>
 
                     <div className="ws-simple-style-cont ws-alt">
                         <div className="container">
-                            <h3>FIZICKA ZASTITA</h3>
-                            <p className="ws-stronger-color">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            <p>Službenici Watchout Security-a prolaze obuku i polaganje iz sledećih oblasti:
                             </p>
-                            <p>
-                                when an unknown printer took a galley of type and scrambled it to make a type specimen
-                                book.
-                                It has survived not only five centuries, but also the leap into electronic typesetting,
-                                remaining essentially unchanged.
-                            </p>
-                            <p>
-                                when an unknown printer took a galley of type and scrambled it to make a type specimen
-                                book.
-                                It has survived not only five centuries, but also the leap into electronic typesetting,
-                                remaining essentially unchanged.
-                            </p>
+                            <ul>
+                                <li>zaštita od požara;</li>
+                                <li>poznavanje pružanja prve pomoći;</li>
+                                <li>poslovna komunikacije;</li>
+                                <li>fizička samoodbrane;</li>
+                                <li>upravljanje sistemima tehničke zaštite.</li>
+                            </ul>
                         </div>
                     </div>
 
                     <div className="ws-simple-style-cont ws-alt ws-triangle-holder ws-triangle-light">
                         <div className="container">
-                            <h3>FIZICKA ZASTITA</h3>
-                            <p className="ws-stronger-color">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                            </p>
-                            <p>
-                                when an unknown printer took a galley of type and scrambled it to make a type specimen
-                                book.
-                                It has survived not only five centuries, but also the leap into electronic typesetting,
-                                remaining essentially unchanged.
-                            </p>
-                            <p>
-                                when an unknown printer took a galley of type and scrambled it to make a type specimen
-                                book.
-                                It has survived not only five centuries, but also the leap into electronic typesetting,
-                                remaining essentially unchanged.
+                            <p>Službenik obezbeđenja u stalnom je kontaktu sa Kontrolnim centrom, čime se osigurava
+                                pravovremenost reakcije u slučaju kriznih situacija
                             </p>
                         </div>
                     </div>
 
                     <div className="ws-simple-style-cont">
                         <div className="container">
-                            <h3>FIZICKA ZASTITA</h3>
-                            <p className="ws-stronger-color">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            <p>Poslovi koje mogu obavljaju službenici obezbeđenja se sledeći: službenik obezbeđenja za
+                                čuvanje objekta u izgradnji, službenik obezbeđenja za stacionarno čuvanje, službenik
+                                obezbeđenja za rad recepcionara, službenik obezbeđenja za kontrolu ulaza i izlaza u
+                                objekte koje se obezbeđuju, službenik obezbeđenja za rad u maloprodajnom objektu,
+                                službenik obezbeđenja za obavljanje redarskih poslova.
                             </p>
-                            <p>
-                                when an unknown printer took a galley of type and scrambled it to make a type specimen
-                                book.
-                                It has survived not only five centuries, but also the leap into electronic typesetting,
-                                remaining essentially unchanged.
+                        </div>
+                    </div>
+
+                    <div className="ws-simple-style-cont">
+                        <div className="container">
+                            <p className="ws-stronger-color">GARANCIJE OBEŠTEĆENJA
                             </p>
-                            <p>
-                                when an unknown printer took a galley of type and scrambled it to make a type specimen
-                                book.
-                                It has survived not only five centuries, but also the leap into electronic typesetting,
-                                remaining essentially unchanged.
+                            <p>U slučaju propusta službe obezbeđenja, nudimo posebne uslove obeštećenja oštećenog
+                                korisnika usluge aktiviranjem adekvatne polise osiguranja.
+                                <br></br>
+                                <br></br>
+                                Watchout Security poseduje Polisu osiguranja od profesionalne odgovornosti za sve
+                                eventualne propuste službenika obezbeđenja. Bez obzira na broj štetnih događaja, polisa
+                                nije iscrpiva (svi štetni događaji do vrednosti od 200.000 evra tokom trajanja polise
+                                biće nadoknađeni svim korisnicima usluga na teritoriji Srbije).
                             </p>
                         </div>
                     </div>
